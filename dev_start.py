22a12f87ba4e25c92c1615caea42040c1a9a1ba2
mikezalik <mike.zalik@gmail.com>
2022-03-23 11:05:48 -0700

    Adds slack subprocess

--- dev_start.py
+++ dev_start.py
@@ -4,6 +4,8 @@ import subprocess
 
 print("Hello Michael, let me start the apps you need...")
 
+subprocess.run("/snap/bin/spotify")
 subprocess.run("/snap/bin/chromium")
 subprocess.run("/snap/bin/code")
-subprocess.run("/snap/spotify/current/usr/bin/spotify")
+subprocess.run("/snap/bin/slack")
+
