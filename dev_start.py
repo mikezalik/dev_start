f1fef3077fdb4a5a0a061c1b296f93cce3a0eceb
mikezalik <mike.zalik@gmail.com>
2022-03-22 20:39:17 -0700

    Adds simple script to start apps for dev work

---
+++ dev_start.py
@@ -0,0 +1,9 @@
+# quick script to speed up app start up for dev work
+
+import subprocess
+
+print("Hello Michael, let me start the apps you need...")
+
+subprocess.run("/snap/bin/chromium")
+subprocess.run("/snap/bin/code")
+subprocess.run("/snap/spotify/current/usr/bin/spotify")
